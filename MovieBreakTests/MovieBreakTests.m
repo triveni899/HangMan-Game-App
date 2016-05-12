87e660658be815141dc22c9cb7a59f307c8ed12d
triveni89 <tbanpel1@binghamton.edu>
2016-05-11 23:54:35 -0400

    Initial Commit

---
+++ MovieBreakTests/MovieBreakTests.m
@@ -0,0 +1,39 @@
+//
+//  MovieBreakTests.m
+//  MovieBreakTests
+//
+//  Created by Triveni Banpela on 5/11/16.
+//  Copyright © 2016 Triveni Banpela. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MovieBreakTests : XCTestCase
+
+@end
+
+@implementation MovieBreakTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
